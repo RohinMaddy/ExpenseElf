78aecc06c89a66c9fed52cce00e296a5302c4e02
Rohin Madhavan <rohinmadhavan96@gmail.com>
2024-05-23 11:54:00 +0100

    Initial Commit

---
+++ ExpenseElf/ExpenseElfApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseElfApp.swift
+//  ExpenseElf
+//
+//  Created by Rohin Madhavan on 23/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseElfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
